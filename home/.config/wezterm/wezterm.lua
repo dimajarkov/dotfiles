e01f37c1db684166f39bf2b817f1353932a11903
dimajarkov <dmitri@arenacrm.com>
2026-08-31 07:04:40 +0930

    Restore WezTerm settings and preserve hide-others shortcut

--- home/.config/wezterm/wezterm.lua
+++ home/.config/wezterm/wezterm.lua
@@ -23,11 +23,54 @@ wezterm.on('update-status', function(window)
   apply_appearance(window)
 end)
 
-config.color_scheme = color_scheme_for_appearance(wezterm.gui.get_appearance())
+local initial_appearance = 'Dark'
+if wezterm.gui then
+  initial_appearance = wezterm.gui.get_appearance()
+end
+config.color_scheme = color_scheme_for_appearance(initial_appearance)
 config.status_update_interval = 500
 config.font = wezterm.font 'Hack Nerd Font'
 config.font_size = 14.0
 config.hide_tab_bar_if_only_one_tab = true
 config.window_decorations = 'RESIZE'
 config.window_background_opacity = 0.98
+
+-- Keep the content-only window and the Hide Others shortcut.
+config.enable_tab_bar = false
+
+local hide_other_apps = [[
+tell application "System Events"
+    set frontmostProcess to first application process whose frontmost is true
+    set frontmostName to name of frontmostProcess
+    repeat with processRef in application processes
+        try
+            if visible of processRef and name of processRef is not frontmostName then
+                set visible of processRef to false
+            end if
+        end try
+    end repeat
+end tell
+]]
+
+local hide_other_apps_action = wezterm.action_callback(function()
+    wezterm.run_child_process({ 'osascript', '-e', hide_other_apps })
+end)
+
+-- Extend WezTerm's defaults instead of replacing its built-in shortcuts.
+local keys = {}
+if wezterm.gui and wezterm.gui.default_keys then
+  keys = wezterm.gui.default_keys()
+end
+table.insert(keys, {
+    key = 'h',
+    mods = 'ALT|SUPER',
+    action = hide_other_apps_action,
+})
+table.insert(keys, {
+    key = 'h',
+    mods = 'ALT|CTRL',
+    action = hide_other_apps_action,
+})
+config.keys = keys
+
 return config
